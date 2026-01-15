eb2eb3b9829e1f968b0e815cc217dfcbaeccbecc
Yuliia Zahoruulko <yzahorulko@gmail.com>
2026-01-15 21:05:29 +0200

    rewrite code without subprocesses

--- app/main.py
+++ app/main.py
@@ -1,20 +1,19 @@
-import subprocess
-
-
 def copy_file(command: str) -> None:
     try:
         command_for_run, source_file, destination = command.split(" ")
     except ValueError:
-        print("command = '' and nothing can be copied")
         return None
 
     if source_file == destination or command_for_run != "cp":
         return None
 
     try:
-        subprocess.run([command_for_run, source_file, destination], check=True)
-        print("Bash cp command executed successfully.")
-    except subprocess.CalledProcessError as e:
-        print(f"Error executing bash command: {e}")
+        with open(source_file, "r") as file_in, \
+                open(destination, "a") as file_out:
+            content = file_in.read()
+            print(content)
+            file_out.write(content)
     except FileNotFoundError:
-        print("Error: might be an issue with system path or OS.")
+        return None
+    except PermissionError:
+        return None
